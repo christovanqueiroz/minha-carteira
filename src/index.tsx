829ab3032ea7e1dca4ee9ac0f5f56a3c2fe2b9d9
christovanqueiroz <christovanbrumqueiroz@gmail.com>
2023-02-14 16:17:35 -0300

    modified:   package-lock.json 	modified:   package.json 	new file:   src/App.tsx 	modified:   src/index.tsx     new file:   src/styles/GlobalStyles.ts 	modified:   tsconfig.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Dashboard from './pages/Dashboard';
-import List from './pages/List';
-import SingIn from './pages/SignIn';
-
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Dashboard />
-    <List />
-    <SingIn />
-
+    <App />
   </React.StrictMode>
-);
+)
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,7 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    width: 100%;
+    height: 100vh;
+    background-color: red;
+`;
