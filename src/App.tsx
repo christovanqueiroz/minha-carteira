173fe38077c1ca200046eac14287fc089d2ec171
christovanqueiroz <christovanbrumqueiroz@gmail.com>
2023-04-11 16:59:52 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.tsx 	new file:   src/routes/app.routes.tsx 	new file:   src/routes/index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,8 @@ type Props = {
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/GlobalStyles';
 
-import Layout from './components/Layout';
-import Dashboard from './pages/Dashboard';
-import List from './pages/List';
+import Routes from './routes'
+
 import dark from './styles/themes/dark';
 import light from './styles/themes/light';
 
@@ -17,9 +16,7 @@ const App: React.FC = () => {
     return (
         <ThemeProvider theme={dark}>
             <GlobalStyles />
-            <Layout>
-                <List />
-            </Layout>
+            <Routes />
         </ThemeProvider>
     )
 }
